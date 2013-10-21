74be3cd7b1439661a56915ad7a08b1624d53f765
WrinklyNinja <wrinklyninja1@gmail.com>
2013-10-21 18:53:40 +0100

    Removed old code, started adding data fetch.
    This should work, but is untested as Bugzilla is currently down due to a
    server move.

--- bugzilla/bugzilla.js
+++ bugzilla/bugzilla.js
@@ -1,118 +1,129 @@
 'use strict';
+var gameSelect = document.getElementById('gameSelect');
+var searchButton = document.getElementById('searchButton');
+var resultsDiv = document.getElementById('results');
 var url = 'http://bugzilla.darkcreations.org/jsonrpc.cgi';
-function getBugId(plugin, desc, xhr) {
-    var request = {
-        "method": "Bug.search",
-        "params": [{
-            "Bugzilla_login": "bossguest@darkcreations.org",
-            "Bugzilla_password": "bosspassword",
-            "product": "BOSS",
-            "component": "TES V: Skyrim",
-            "summary": plugin
-        }],
-        "id": 1
-    };
-    outputPluginSubmitText('Checking for existing submission...', 0);
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onerror = pluginSubmitError;
-    xhr.onload = function() {
-        if (xhr.status == 200 && isResponseOK(xhr.responseText)) {
-            var response = JSON.parse(xhr.responseText);
-            if (response.result.bugs.length > 0) {
-                var id = response.result.bugs[0].id;
-                addBugComment(id, desc, xhr);
-            } else {
-                addBug(plugin, desc, xhr);
+var games = [
+    "TES IV: Oblivion",
+    "TES V: Skyrim",
+    "Fallout 3",
+    "Fallout: New Vegas"
+];
+var hashmap = {};
+
+function outputBugData() {
+    for (var i in hashmap) {
+        console.log(hashmap[i]);
+    }
+}
+
+function processBugComments() {
+    if (this.status == 200 && JSON.parse(this.responseText).error == null) {
+        var response = JSON.parse(this.responseText);
+        if (response.result.bugs.length > 0) {
+            for (var i in response.result.bugs) {
+                hashmap[i].comments = response.result.bugs[i].comments;
             }
-        } else {
-            outputPluginSubmitText('Error: Existing submission check failed!', -1);
+            outputBugData();
+        } else { 
+            console.log("No bug comments found.");
+            resultsDiv.textContent = "No bug comments found.";
         }
-    };
-    xhr.send(JSON.stringify(request));
+    } else {
+        console.log("Error while getting bug comments list.");
+        resultsDiv.textContent = "Error while getting bug comments list.";
+    }
 }
-function addBugComment(id, comment, xhr) {
-    var request = {
-        "method": "Bug.add_comment",
-        "params": [{
-            "Bugzilla_login": "bossguest@darkcreations.org",
-            "Bugzilla_password": "bosspassword",
-            "id": id,
-            "comment": comment
-        }],
-        "id": 2
-    };
-    outputPluginSubmitText('Previous submission found, updating with supplied details...', 0);
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onerror = pluginSubmitError;
-    xhr.onload = function() {
-        if (xhr.status == 200 && isResponseOK(xhr.responseText)) {
-            outputPluginSubmitText('Plugin submission updated!', 1);
-        } else {
-            outputPluginSubmitText('Error: Plugin submission could not be updated.', -1);
+
+function processBugIDs() {
+    if (this.status == 200 && JSON.parse(this.responseText).error == null) {
+        var response = JSON.parse(this.responseText);
+        if (response.result.bugs.length > 0) {
+            /* Now we have an array of bug objects. However, the info in these objects doesn't include comments, so that'll have to be gotten for the bugs. */
+            var ids = [];
+            for (var i=0; i < response.result.bugs.length; ++i) {
+                var id = response.result.bugs[i].id;
+                ids[i] = id;
+                hashmap[id] = {
+                    "name": response.result.bugs[i].summary,
+                    "comments": []
+                };
+                console.log("\t" + response.result.bugs[i].id);
+            }
+            
+            var req = {
+                "Bugzilla_login": "bossguest@darkcreations.org",
+                "Bugzilla_password": "bosspassword",
+                "method": "Bug.comments",
+                "id": 2,
+                "params": [{
+                    "ids": ids
+                }]
+            };
+            
+            console.log("Getting comments for bugs...");
+            resultsDiv.textContent = "Getting comments for bugs...";
+            
+            try {
+                this.onload = processBugComments;
+                this.open('POST', url, true);
+                this.setRequestHeader('Content-Type', 'application/json');
+                this.send(JSON.stringify(req));
+            } catch (err) {
+                console.log("Error while getting bug list: " + err.message);
+                resultsDiv.textContent = "Error while getting bug list: " + err.message;
+            }
+            
+        } else { 
+            console.log("No bugs found.");
+            resultsDiv.textContent = "No bugs found.";
         }
-    };
-    xhr.send(JSON.stringify(request));
+    } else {
+        console.log("Error while getting bug list.");
+        resultsDiv.textContent = "Error while getting bug list.";
+    }
 }
-function addBug(summary, description, xhr) {
-    var request = {
-        "method": "Bug.create",
+
+function onExtractInit(evt) {
+
+    if (evt.keyCode != 0 && evt.keyCode != 13) {
+        return;
+    }
+    
+    var req = {
+        "Bugzilla_login": "bossguest@darkcreations.org",
+        "Bugzilla_password": "bosspassword",
+        "method": "Bug.search",
+        "id": 1,
         "params": [{
-            "Bugzilla_login": "bossguest@darkcreations.org",
-            "Bugzilla_password": "bosspassword",
             "product": "BOSS",
-            "component": "TES V: Skyrim",
-            "summary": summary,
-            "version": "2.1",
-            "description": description,
-            "op_sys": "Windows",
-            "platform": "PC",
-            "priority": "---",
-            "severity": "enhancement"
-        }],
-        "id": 3
+            "component": gameSelect.value,
+            "limit": 0,
+        }]
     };
-    outputPluginSubmitText('No previous submission found, creating new submission...', 0);
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onerror = pluginSubmitError;
-    xhr.onload = function() {
-        if (xhr.status == 200 && isResponseOK(xhr.responseText)) {
-            outputPluginSubmitText('Plugin submitted!', 1);
-        } else {
-            outputPluginSubmitText('Error: Plugin could not be submitted.', -1);
-        }
-    };
-    xhr.send(JSON.stringify(request));
-}
-function pluginSubmitError() {
-    outputPluginSubmitText('Error: Data transfer failed.', -1);
-}
-function isResponseOK(text) {
-    return (JSON.parse(text).error == null);
-}
-function HTMLToJSON(text) {
-    return text.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '\"').replace(/&#039;/g, '\'');
-}
-function submitPlugin(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-    if (evt.currentTarget[0].value.length == 0 && evt.currentTarget[1].value.length == 0) {
-        outputPluginSubmitText('Please supply at least a link or some notes.', -2);
-        return;
-    } else if (isValidationSupported() && !evt.currentTarget.checkValidity()) {
-        return;
-    }
-    var desc = evt.currentTarget[0].value;
-    if (desc.length != 0) {
-        desc += '\n\n';
-    }
-    desc += evt.currentTarget[1].value;
+
+    console.log("Getting bug list...");
+    resultsDiv.textContent = "Getting bug list...";
+    
     try {
         var xhr = new XMLHttpRequest();
-        getBugId(HTMLToJSON(document.getElementById('plugin').textContent), HTMLToJSON(desc), xhr);
+        xhr.onload = processBugIDs;
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(req));
     } catch (err) {
-        outputPluginSubmitText('Exception occurred: ' + err.message, -1);
+        console.log("Error while getting bug list: " + err.message);
+        resultsDiv.textContent = "Error while getting bug list: " + err.message;
     }
 }
+
+/* Fill the drop-down games box with stuff. */
+for (var i=0; i < games.length; ++i) {
+    var option = document.createElement('option');
+    option.innerText = games[i];
+    option.setAttribute('value', games[i]);
+    gameSelect.appendChild(option);
+}
+
+searchButton.addEventListener("click", onExtractInit, false);
+document.body.addEventListener("keypress", onExtractInit, false);
